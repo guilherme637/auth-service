a340a3e2269a9a901a0f530121fb621b9f280262
gui_chaves37 <guilhermejr296@gmail.com>
2023-08-15 22:21:04 -0300

    Ajustando form

--- src/Controller/GetLoginAction.php
+++ src/Controller/GetLoginAction.php
@@ -3,13 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GetLoginAction extends AbstractController
 {
-    #[Route(path: '/login')]
-    public function __invoke()
+    #[Route(path: '/login', methods: ['POST', 'GET'])]
+    public function __invoke(Request $request)
     {
-        return $this->render('base.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->getForm()->createView()
+        ;
+
+        if ($request->isMethod('POST')) {
+            dump($request);
+            exit();
+
+        }
+        //TODO ajustar no campos do form type e depois fazer as validações ncessarias
+        return $this->render('base.html.twig', ['forms' => $form]);
     }
 }
